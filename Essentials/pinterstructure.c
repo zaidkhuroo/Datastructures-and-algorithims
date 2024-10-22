b00e207e17a1aba7e70797a344560068c1e37600
zaidkhuroo <khuroozaid@gmail.com>
2024-10-22 16:02:42 +0530

    Pointer to a structure

---
+++ Essentials/pinterstructure.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+struct triangle
+{
+    int a,b,c;
+};
+int main(){
+    struct triangle *t;
+   t=(struct triangle*)malloc(sizeof(struct triangle));  // creating object of type structure in heap
+    t->a=10;
+    t->b=20;
+    t->c=60;
+    printf("Sides of Triangle are: %d %d %d\n",t->a,t->b,t->c);
+}
