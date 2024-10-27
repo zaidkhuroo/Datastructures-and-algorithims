d2cec4842f5cc15fd6b6a70654463958561df5f6
zaidkhuroo <khuroozaid@gmail.com>
2024-10-27 15:55:25 +0530

    Structure exercise

---
+++ Essentials/strucuture/strucAndFunc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct triangle
+{
+    int a;
+    int b;
+    int c;
+};
+// function to initialize values to the triangle using call by address
+void initialize(struct triangle *t, int l, int m, int n)
+{
+    t->a = l;
+    t->b = m;
+    t->c = n;
+    printf("\n Initializing: %d %d %d", l, m, n);
+}
+
+// function to calculate the perimeter of the triangle using call by value
+int area(struct triangle t)
+{
+    return (t.a * t.b * t.c);
+}
+
+// function to change values to the triangle using call by address
+void change(struct triangle *t, int k)
+{
+    t->a = k;
+}
+
+int main()
+{
+    struct triangle t;
+    initialize(&t, 20, 40, 60);
+    int total = area(t);
+    printf("\n Total area before changing value: %d", total);
+    change(&t, 30);
+    printf("\n Changed value: %d \n", t.a);
+    int total_area = area(t);
+    printf("Total area after changing value: %d", total_area);
+    return 0;
+}
