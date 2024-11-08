077e67a8d62ebaa29eb27d43dcee2d41919dc376
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-11-08 07:26:36 +0000

    Sum of first n natural numbers using recursion

--- Exercise/recursion/sumOfnNaturalNum.c
+++ Exercise/recursion/sumOfnNaturalNum.c
@@ -4,10 +4,21 @@ int sum(int x)
 {
     return x * (x + 1) / 2;
 }
+
+// by recursion
+int sum2(int x)
+{
+    if (x == 0)
+        return 0;
+    return sum2(x - 1) + x;
+}
+
 int main()
 {
-    int a;
+    int a, b;
     a = sum(10);
-    printf("Sum of first natural numbers is:  %d\n", a);
+    b = sum2(10);
+    printf("Sum of first 10 natural numbers is:  %d\n", a);
+    printf("Sum of first 10 natural numbers by recursion is:  %d\n", b);
     return 0;
 }
