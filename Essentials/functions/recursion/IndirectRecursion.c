68008a3c3f361960d4175996c47c7e69b594bbb9
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-11-08 06:08:49 +0000

    Indirect Recursion

---
+++ Essentials/functions/recursion/IndirectRecursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void fun1(int n)
+{
+    if (n > 0)
+    {
+        printf("%d", n);
+        fun2(n - 1);
+    }
+}
+
+
+void fun2(int n)
+{
+    if (n > 0)
+    {
+        printf("%d", n);
+        fun1(n / 2);
+    }
+}
+
+int main(int a)
+{
+    a = 10;
+    fun1(a);
+    return 0;
+}
