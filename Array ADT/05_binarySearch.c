0a08516fe95e9bec6386f6ed297a5fa146fbf4e7
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-12-07 15:07:59 +0000

    Linear Search in Array ADT using Transposition and Move to head technique

--- Array ADT/05_binarySearch.c
+++ Array ADT/05_binarySearch.c
@@ -32,7 +32,8 @@ int search(struct ARR *arr, int num)
     for (i = 0; i < arr->length - 1; i++)
     {
         if (num == arr->A[i])
-            swap(arr->A[i], arr->A[i-1]);  // this will increase efficency of searching the element if it is searched once or for repeated times, more it is searched more its swapped with the previous elemnt and thus reaches at the starting index of the array and the goal is to have the time complexity as O(1);
+            swap(arr->A[i], arr->A[i-1]);  // transposition technique: this will increase efficency of searching the element if it is searched once or for repeated times, more it is searched more its swapped with the previous elemnt and thus reaches at the starting index of the array and the goal is to have the time complexity as O(1);
+            swap(arr->A[i], arr->A[0]);  // Move to front/Head: this will increase efficency of searching the element if it is searched once the element gets swapped with the starting element of the array thus the time complexity becomes O(1) if the same elemnt gets searched again.;
             return i;
     }
     return -1;
