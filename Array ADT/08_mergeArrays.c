1c2e323859da726cb234404a76e57250723b445d
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-12-21 07:40:34 +0000

    Merge arrays

--- Array ADT/08_mergeArrays.c
+++ Array ADT/08_mergeArrays.c
@@ -3,53 +3,47 @@
 
 struct ARR
 {
-    int A[10];
+    int A[20];
     int size;
     int length;
 };
 void display(struct ARR arr)
 {
     int i;
-    printf("Reverse Elements in array are: \n");
+    printf("Elements in array are: \n");
     for (i = 0; i < arr.length; i++)
     {
         printf("%d \t", arr.A[i]);
     }
 }
-void swap(int *x, int *y)
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-int reverse(struct ARR *arr)
-{
-    int i, j;
-    int *B;
 
-    B = (int *)malloc(arr->length * sizeof(int));
-    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
-        B[j] = arr->A[i];
-    for (i = 0; i < arr->length; i++)
-        arr->A[i] = B[i];
-}
-
-int reverseSwap(struct ARR *arr)
+struct ARR *merge(struct ARR *arr1, struct ARR *arr2)
 {
-    int i, j,k;
-    while(i<arr->length)
-}
+    int i, j, k;
+    i = j = k = 0;
+    struct ARR *arr3 = (struct ARR *)malloc(sizeof(struct ARR));
+    while (i < arr1->length && j < arr2->length)
+        if (arr1->A[i] < arr2->A[j])
 
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = arr3->length;
+    return arr3;
+}
 int main()
 {
-    struct ARR arr = {{1, 2, 3, 4, 5, 6, 7, 8}, 10, 8};
-    struct ARR arr2 = {{19,12,34}, 5, 3};  
-    reverse(&arr);
-
-    display(arr);
-    reverseSwap(&arr);
-    printf(" \n With swap \n");
-    display(arr);
+    struct ARR arr1 = {{1, 2, 3, 4, 5, 6, 7, 8}, 10, 8};
+    struct ARR arr2 = {{9, 12, 34}, 10, 3};
+    struct ARR *arr3;
+    arr3 = merge(&arr1, &arr2);
+    display(*arr3);
+    free(arr3);
     return 0;
 }
