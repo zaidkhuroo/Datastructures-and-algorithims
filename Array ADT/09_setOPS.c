57fd7c9a6e30ba84d283b62a1d261986b0bb6cbb
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-12-21 13:58:16 +0000

    Set Operations on arrays

--- Array ADT/09_setOPS.c
+++ Array ADT/09_setOPS.c
@@ -25,7 +25,6 @@ struct ARR *Union(struct ARR *arr1, struct ARR *arr2)
     struct ARR *arr3 = (struct ARR *)malloc(sizeof(struct ARR));
     while (i < arr1->length && j < arr2->length)
         if (arr1->A[i] < arr2->A[j])
-
             arr3->A[k++] = arr1->A[i++];
         else if (arr2->A[j] < arr1->A[i])
             arr3->A[k++] = arr2->A[j++];
@@ -51,16 +50,37 @@ struct ARR *Inter(struct ARR *arr1, struct ARR *arr2)
     struct ARR *arr3 = (struct ARR *)malloc(sizeof(struct ARR));
     while (i < arr1->length && j < arr2->length)
         if (arr1->A[i] < arr2->A[j])
-           i++;
+            i++;
         else if (arr2->A[j] < arr1->A[i])
-           j++;
+            j++;
         else
         {
             arr3->A[k++] = arr1->A[i++];
             j++;
         }
 
-    
+    arr3->length = k;
+    arr3->size = arr3->length;
+    return arr3;
+}
+
+struct ARR *Diff(struct ARR *arr1, struct ARR *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+    struct ARR *arr3 = (struct ARR *)malloc(sizeof(struct ARR));
+    while (i < arr1->length && j < arr2->length)
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else if (arr2->A[j] < arr1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
     arr3->length = k;
     arr3->size = arr3->length;
     return arr3;
@@ -68,13 +88,16 @@ struct ARR *Inter(struct ARR *arr1, struct ARR *arr2)
 
 int main()
 {
-    struct ARR arr1 = {{1, 3, 5, 6, 7, 8,9}, 10, 7};
-    struct ARR arr2 = {{2, 4, 5, 6,7}, 10, 5};
+    struct ARR arr1 = {{1, 3, 5, 6, 7, 8, 9}, 10, 7};
+    struct ARR arr2 = {{2, 4, 5, 6, 7}, 10, 5};
     struct ARR *arr3;
     arr3 = Union(&arr1, &arr2);
     display(*arr3);
     arr3 = Inter(&arr1, &arr2);
     display(*arr3);
+    arr3 = Diff(&arr1, &arr2);
+    display(*arr3);
     free(arr3);
+
     return 0;
 }
