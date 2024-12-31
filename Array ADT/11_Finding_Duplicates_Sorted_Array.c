8797951e9e2a397776d145e5c6a0e6821c007c8e
Zaid Bin Farooq <khuroozaid@gmail.com>
2024-12-31 07:18:59 +0000

    Finding Duplicates in a Sorted array and counting them

--- Array ADT/11_Finding_Duplicates_Sorted_Array.c
+++ Array ADT/11_Finding_Duplicates_Sorted_Array.c
@@ -10,7 +10,7 @@ struct ARR
 void display(struct ARR arr)
 {
     int i;
-    printf("Elements in array are: \n");
+    printf("Elements in array are: \t");
     for (i = 0; i < arr.length; i++)
     {
         printf("%d \t", arr.A[i]);
@@ -19,25 +19,29 @@ void display(struct ARR arr)
 
 int Duplicates(struct ARR *arr)
 {
-    int i, sum = 0, totalsum, missing, last = 0;
+    int i, sum = 0, totalsum, missing, last = 0, count = 0;
+    ;
     int n = arr->A[arr->length - 1];
 
     for (i = 0; i < arr->length; i++)
     {
 
         if (arr->A[i] == arr->A[i + 1] && arr->A[i] != last)
-        
+        {
+
             printf("%d \t", arr->A[i]);
-        
+            count++;
+        }
         last = arr->A[i];
-       
     }
+    printf("\n Repeating Elements are: %d \t", count);
 }
 
 int main()
 {
-    struct ARR arr = {{1, 3, 4, 4, 6, 7, 7, 7, 8, 8}, 10, 10};
-    printf(" Duplicate numbers are: ");
+    struct ARR arr = {{1, 1, 3, 4, 4, 6, 7, 7, 8, 8}, 10, 10};
+    display(arr);
+    printf(" \n Duplicate numbers are: ");
     Duplicates(&arr);
     return 0;
 }
